3b47894a325372b3afe11d7017a6c195d8f1d824
Roland <cook.rolandcook.roland@gmail.com>
2023-07-24 20:31:11 -0400

    Service view function wrote

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,64 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=200)
+    sold = models.BooleanField()
+
+
+class Status(models.Model):
+    id = models.PositiveSmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+    
+
+
+
+class Technician(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    employee_id = models.CharField(max_length=200)
+
+
+class Appointment(models.Model):
+
+    @classmethod
+    def create(cls, **kwargs):
+        kwargs["status"] = Status.objects.get(name="SUBMITTED")
+        appointment = cls(**kwargs)
+        appointment.save()
+        return appointment 
+
+
+
+    date_time = models.DateField(blank=False)
+    reason = models.TextField()
+    vin = models.CharField(max_length=200)
+    customer = models.CharField(max_length=200)
+    
+    technician = models.ForeignKey(
+        Technician,
+        related_name="technican",
+        on_delete=models.CASCADE
+    )
+
+    status = models.ForeignKey(
+        Status,
+        related_name="appointment",
+        on_delete=models.PROTECT,
+    )
+
+
+
+    def cancelled(self):
+        status = Status.objects.get(name="cancelled")
+        self.status = status
+        self.save()
+
+    def finished(self):
+        status = Status.objects.get(name="finished")
+        self.status = status
+        self.save()
---
+++ service/api/service_rest/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import api_technicians, api_tech_detail, api_appointments, api_appointment_details, api_cancel, api_finished
+
+urlpatterns = [
+    path("technicians/", api_technicians, name="api_technicians"),
+    path("technicians/<int:id>", api_tech_detail, name="api_tech_detail"),
+    path("appointments/",api_appointments, name="api_appointments"),
+    path("appointments/<int:id>",api_appointment_details, name="api_appointment_details"),
+    path("appointments/<int:id>/cancel", api_cancel, name="api_cancel"),
+    path("appointments/<int:id>/finish", api_finished, name="api_finish")
+
+    
+
+]
