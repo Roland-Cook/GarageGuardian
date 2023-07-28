126b6936eafe1392e213733e11459d0fae5af020
Roland <cook.rolandcook.roland@gmail.com>
2023-07-28 15:50:30 -0400

    Improved error handling

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -63,12 +63,18 @@ def api_technicians(request):
 @require_http_methods(["GET", "DELETE"])
 def api_tech_detail(request, id):
     if request.method == "GET":
-        tech = Technician.objects.get(id=id)
-        return JsonResponse(
-            tech,
-            encoder=TechnicianEncoder,
-            safe=False,
-        )
+        try:  
+            tech = Technician.objects.get(id=id)
+            return JsonResponse(
+                tech,
+                encoder=TechnicianEncoder,
+                safe=False,
+            )
+        except Technician.DoesNotExist:
+                return JsonResponse(
+                {"message": "Cannot find technician"},
+                status=400
+            )
     else:
         count, _ = Technician.objects.filter(id=id).delete()
         return JsonResponse({"deleted": count > 0})
@@ -77,11 +83,17 @@ def api_tech_detail(request, id):
 @require_http_methods(["GET", "POST"])
 def api_appointments(request):
         if request.method == "GET":
-            appointments = Appointment.objects.all()
-            return JsonResponse(
-            {"appointments":appointments},
-            encoder=AppointmentEncoder,
-            safe=False
+            try:
+                appointments = Appointment.objects.all()
+                return JsonResponse(
+                {"appointments":appointments},
+                encoder=AppointmentEncoder,
+                safe=False
+                )
+            except Appointment.DoesNotExist:
+                return JsonResponse(
+                {"message": "Cannot find Appointments"},
+                status=404
             )
         else:
             content = json.loads(request.body)
@@ -90,10 +102,9 @@ def api_appointments(request):
                 content["technician"] = technician
             except Technician.DoesNotExist:
                 return JsonResponse(
-                {"message": "Cannot Create Appointment"},
+                {"message": "Cannot Find technician"},
                 status=400,
                 )
-
             appointment = Appointment.create(**content)
             return JsonResponse(
             appointment,
@@ -105,12 +116,18 @@ def api_appointments(request):
 @require_http_methods(["GET", "DELETE"])
 def api_appointment_details(request, id):
     if request.method == "GET":
-        app = Appointment.objects.get(id=id)
-        return JsonResponse(
-            app,
-            encoder=AppointmentEncoder,
-            safe=False,
-        )
+        try:
+            app = Appointment.objects.get(id=id)
+            return JsonResponse(
+                app,
+                encoder=AppointmentEncoder,
+                safe=False,
+            )
+        except Appointment.DoesNotExist:
+                return JsonResponse(
+                {"message": "No appointment found"},
+                status=400
+            )
     else:
         count, _ = Appointment.objects.filter(id=id).delete()
         return JsonResponse({"deleted": count > 0})
@@ -118,21 +135,33 @@ def api_appointment_details(request, id):
 
 @require_http_methods(["PUT"])
 def api_cancel(request,id):
-    app = Appointment.objects.get(id=id)
-    app.cancelled()
-    return JsonResponse(
-        app,
-        encoder=AppointmentEncoder,
-        safe=False,
-    )
+    try:
+        app = Appointment.objects.get(id=id)
+        app.cancelled()
+        return JsonResponse(
+            app,
+            encoder=AppointmentEncoder,
+            safe=False,
+        )
+    except Appointment.DoesNotExist:
+            return JsonResponse(
+                {"message": "Cannot change status to cancelled"},
+                status=400
+            )
 
 
 @require_http_methods(["PUT"])
 def api_finished(request,id):
-    app = Appointment.objects.get(id=id)
-    app.finished()
-    return JsonResponse(
-        app,
-        encoder=AppointmentEncoder,
-        safe=False
-    )
+    try:
+        app = Appointment.objects.get(id=id)
+        app.finished()
+        return JsonResponse(
+            app,
+            encoder=AppointmentEncoder,
+            safe=False
+        )
+    except Appointment.DoesNotExist:
+            return JsonResponse(
+                {"message": "Cannot change status to finished"},
+                status=400
+            )
